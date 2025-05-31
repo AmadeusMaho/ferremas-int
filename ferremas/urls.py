11843adbd2cfe7651dc322d1f9ff8c7b8771ef90
Mauri <maur.gatica@duocuc.cl>
2025-05-31 13:43:38 -0400

    first html y vista

--- ferremas/urls.py
+++ ferremas/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ferremasDjango import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.verIndex)
 ]
