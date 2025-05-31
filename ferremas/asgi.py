b675f5f734522bd15afb67c661494193d66becca
Mauri <maur.gatica@duocuc.cl>
2025-05-31 13:21:58 -0400

    fix

--- ferremas/asgi.py
+++ ferremas/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ferremasDjango project.
+ASGI config for ferremas project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ferremasDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ferremas.settings')
 
 application = get_asgi_application()
--- ferremas/wsgi.py
+++ ferremas/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ferremasDjango project.
+WSGI config for ferremas project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ferremasDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ferremas.settings')
 
 application = get_wsgi_application()
